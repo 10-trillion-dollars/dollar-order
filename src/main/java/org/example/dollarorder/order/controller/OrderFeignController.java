81994a5391cdcf70220f513171e0cb9dd549cdf0
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-12 16:40:26 +0900

    :bug: 깃허브 충돌 해결

--- src/main/java/org/example/dollarorder/order/controller/OrderFeignController.java
+++ src/main/java/org/example/dollarorder/order/controller/OrderFeignController.java
@@ -1,9 +1,12 @@
 package org.example.dollarorder.order.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.dollarorder.order.entity.OrderDetail;
 import org.example.dollarorder.order.service.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +19,15 @@ public class OrderFeignController {
     Long countByUserIdAndProductId(@PathVariable Long userId, @PathVariable Long productId){
         return orderService.countByUserIdAndProductId(userId, productId);
     }
-//    @GetMapping("/order/users/{userId}/products/{productId}")
-//    String checkOrderState(@PathVariable Long userId,@PathVariable Long productId) {
-//        return orderService.checkOrderState(userId,productId);
-//    }
+
+    @GetMapping("/users/{userId}/products/{productId}/orders")
+    List<OrderDetail> getOrderDetails(@PathVariable Long userId, @PathVariable Long productId){
+        return orderService.getOrderDetails(userId, productId);
+    }
+
+    @PostMapping("/orders/orderDetail/reviewState")
+    void saveOrderDetailReviewedState(OrderDetail orderDetail){
+        orderService.saveOrderDetailReviewedState(orderDetail);
+    }
+
 }
