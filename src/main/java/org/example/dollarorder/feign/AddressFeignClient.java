3121e9ec87a4bd077ffbbad5c50d15eb8ab0daf2
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-14 12:06:47 +0900

    :bug: CORS 로컬 테스트

--- src/main/java/org/example/dollarorder/feign/AddressFeignClient.java
+++ src/main/java/org/example/dollarorder/feign/AddressFeignClient.java
@@ -3,11 +3,12 @@ package org.example.dollarorder.feign;
 import org.example.dollarorder.domain.address.entity.Address;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "dollar-user", url = "http://localhost:8082/external")
 public interface AddressFeignClient {
 
     @GetMapping("/address/{addressId}")
-    Address findOne(Long addressId);
+    Address findOne(@PathVariable Long addressId);
 
 }
